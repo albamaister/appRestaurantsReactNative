67c8c80558ef5081ed85c1ef7e6deaaa493c6fee
Andres Alba <bryanalba92@gmail.com>
2020-05-23 13:07:06 -0500

    Showing different Screen if the user is logged in or not

--- app/screens/Account/UserGuest.js
+++ app/screens/Account/UserGuest.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-export default function Account() {
+export default function UserGuest() {
     
     return (
         <View>
-            <Text>Account...</Text>
+            <Text>UserGuest...</Text>
         </View>
     )
 
