d401e347bbaf80aed4c0a6f976994ccbc952954d
Andres Alba <bryanalba92@gmail.com>
2020-05-28 00:17:17 -0500

    Login with firebase

--- app/screens/Account/Login.js
+++ app/screens/Account/Login.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import { StyleSheet, View, ScrollView, Text, Image} from 'react-native';
 import { Divider } from 'react-native-elements';
 import { useNavigation } from '@react-navigation/native';
+import LoginForm from '../../components/Account/LoginForm';
+import Toast from 'react-native-easy-toast';
 
 export default function Login() {
+    const toastRef = useRef();
     return (
         <ScrollView>
             <Image 
@@ -11,11 +14,12 @@ export default function Login() {
                 resizeMode='contain'
                 style={styles.logo}/>
                 <View style={styles.viewContainer}>
-                    <Text>Login Form</Text>
+                    <LoginForm toastRef={toastRef}/>
                     <CreateAccount/>
                 </View>
                 <Divider style={styles.divider}/>
                 <Text>Social Login</Text>
+                <Toast ref={toastRef} position='center' opacity={0.9}/>
         </ScrollView>
     )
 }
