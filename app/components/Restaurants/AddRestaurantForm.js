e8ee5068c98909067161585cfd502bcba807dada
Andres Alba <bryanalba92@gmail.com>
2020-06-26 00:13:14 -0500

    Selected images from galery

--- app/components/Restaurants/AddRestaurantForm.js
+++ app/components/Restaurants/AddRestaurantForm.js
@@ -1,30 +1,111 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View, ScrollView, Alert, Dimensions } from 'react-native';
 import { Icon, Avatar, Image, Input, Button } from 'react-native-elements';
+import * as Permissions from 'expo-permissions';
+import * as ImagePicker from 'expo-image-picker';
 
-export default function AddRestaurantForm() {
+export default function AddRestaurantForm(props) {
+
+    const { toastRef, setIsLoading, navigation } = props;
+
+    const [restaurantName, setRestaurantName] = useState('');
+    const [restaurantAddress, setRestaurantAddress] = useState('');
+    const [restaurantDescription, setRestaurantDescription] = useState('');
+    const [imageSelected, setImageSelected] = useState([]);
+
+    console.log(imageSelected);
+
+    const addRestaurant = () => {
+        console.log('ok!!');
+        console.log(restaurantName);
+        console.log(restaurantAddress);
+        console.log(restaurantDescription);
+    }
+
     return(
         <ScrollView style={styles.scrollView}>
-           <FormAdd/>
+           <FormAdd
+            setRestaurantName={setRestaurantName}
+            setRestaurantAddress={setRestaurantAddress}
+            setRestaurantDescription={setRestaurantDescription}
+           />
+           <UploadImage
+                toastRef={toastRef}
+                imageSelected={imageSelected}
+                setImageSelected={setImageSelected}
+           />
+           <Button 
+                title='Crear restaurante'
+                onPress={addRestaurant}
+                buttonStyle={styles.btnAddRestaurant}
+            />
         </ScrollView>
     )
 }
 
 function FormAdd(props) {
+
+    const { setRestaurantName, setRestaurantAddress, setRestaurantDescription } = props;
     return (
         <View styles={styles.viewForm}>
             <Input
                 placeholder='Nombre del restaurante'
                 containerStyle={styles.input}
+                onChange={e => setRestaurantName(e.nativeEvent.text)}
             />
             <Input
                 placeholder='Direccion'
                 containerStyle={styles.input}
+                onChange={e => setRestaurantAddress(e.nativeEvent.text)}
             />
             <Input
                 placeholder='Descripcion del restaurante'
                 multiline={true}
                 inputContainerStyle={styles.textArea}
+                onChange={e => setRestaurantDescription(e.nativeEvent.text)}
+            />
+        </View>
+    )
+}
+
+function UploadImage(props) {
+
+    const { toastRef, setImageSelected, imageSelected } = props;
+
+    const imageSelect = async () => {
+        const resultPermissons = await Permissions.askAsync(
+            Permissions.CAMERA_ROLL
+        );
+        if ( resultPermissons === 'denied' ) {
+            toastRef.current.show(
+                'Es necesario aceptar los permisos de galeria, si los has rechazado tienes que ir a los ajustes y activarlos', 3000
+            );
+        } else {
+            const result = await ImagePicker.launchImageLibraryAsync({
+                allowsEditing: true,
+                aspect: [4, 3]
+            });
+
+            if ( result.cancelled ) {
+                toastRef.current.show(
+                    'Has cerrado la galeria sin seleccionar ninguna imagen', 2000
+                )
+            } else {
+                console.log(result.uri);
+                // setImageSelected(result.uri);
+                setImageSelected(...imageSelected, result.uri);
+            }
+        }
+    }
+
+    return(
+        <View style={styles.viewImages}>
+            <Icon
+                type='material-community'
+                name='camera'
+                color='#7a7a7a'
+                containerStyle={styles.containerIcon}
+                onPress={imageSelect}
             />
         </View>
     )
@@ -46,5 +127,23 @@ const styles = StyleSheet.create({
         width: '100%',
         padding: 0,
         margin: 0
+    },
+    btnAddRestaurant: {
+        backgroundColor: '#00a680',
+        margin: 20
+    },
+    viewImages: {
+        flexDirection: 'row',
+        marginLeft: 20,
+        marginRight: 20,
+        marginTop: 30
+    },
+    containerIcon: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginRight: 10,
+        height: 70,
+        width: 70,
+        backgroundColor: '#e3e3e3'
     }
 });
