db1a2d0f3123be05bff25a6cd3b280f68292a95d
Andres Alba <bryanalba92@gmail.com>
2020-07-17 00:21:13 -0500

    Carousel

--- app/screens/Restaurants/Restaurant.js
+++ app/screens/Restaurants/Restaurant.js
@@ -1,16 +1,26 @@
 import React, { useState, useEffect } from 'react'
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native';
+import { Rating } from 'react-native-elements';
+import Loading from '../../components/Loading';
+import Carousel from '../../components/Carousel';
+
 import { firebaseApp } from '../../utils/firebase';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
+
 const db = firebase.firestore(firebaseApp);
+const screenWidth = Dimensions.get('window').width;
 
 export default function Restaurant(props) {
 
     
     const {navigation, route} = props;
     const { id, name } = route.params;
-    const [retaurant, setRetaurant] = useState(null);
+    const [restaurant, setRestaurant] = useState(null);   
+    const [rating, setRating] = useState(0);
+    
+    
+    console.log(restaurant);
 
     navigation.setOptions({title: name});
 
@@ -20,15 +30,65 @@ export default function Restaurant(props) {
             .then((response) => {
                 const data = response.data();
                 data.id = response.id;
-                setRetaurant(data);
+                setRestaurant(data);
+                setRating(data.rating);
             })
     }, [])
 
+    if (!restaurant) return <Loading isVisible={true} text='Cargando...'/>
+
     return (
-        <View>
-            <Text>Restaurante info</Text>
+        <ScrollView vertical style={styles.viewBody}>
+            <Carousel
+                arrayImages={restaurant.images}
+                height={250}
+                width={screenWidth}
+            />
+            <TitleRestaurant
+                name={restaurant.name}
+                description={restaurant.description}
+                rating={restaurant.rating}
+            />
+        </ScrollView>
+    )
+}
+
+function TitleRestaurant(props) {
+    const { name, description, rating } = props;
+    return (
+        <View style={styles.viewRestaurantTitle}>
+            <View style={{ flexDirection: 'row' }}>
+                <Text style={styles.nameRestaurant}>{name}</Text>
+                <Rating
+                    style={styles.rating}
+                    imageSize={20}
+                    readonly
+                    startingValue={parseFloat(rating)} 
+                />
+            </View>
+            <Text style={styles.descriptionRestaurant}>{description}</Text>
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    viewBody: {
+        flex: 1,
+        backgroundColor: '#fff'
+    },
+    viewRestaurantTitle: {
+        padding: 15
+    },
+    nameRestaurant: {
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
+    descriptionRestaurant: {
+        marginTop: 5,
+        color: 'gray'
+    },
+    rating: {
+        position: 'absolute',
+        right: 0
+    }
+})
