c9946988b25f041b0f248ce5f7993a55447e5a06
Andres Alba <bryanalba92@gmail.com>
2020-07-19 00:19:49 -0500

    Open GPS

--- app/components/Map.js
+++ app/components/Map.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import MapView from 'react-native-maps';
+import openMap from 'react-native-open-maps';
 
 export default function Map(props) {
     const { location, name, height } = props;
+
+    const openAppMap = () => {
+        openMap({
+            latitude: location.latitude,
+            longitude: location.longitude,
+            zoom: 19,
+            query: name
+        })
+    }
     return (
-        <MapView style={{height: height, width: '100%'}} initialRegion={location}>   
+        <MapView onPress={openAppMap} style={{height: height, width: '100%'}} initialRegion={location}>   
             <MapView.Marker coordinate={{latitude: location.latitude, longitude: location.longitude}}/>         
         </MapView>
     )
