d397b55c55c5acd8ec221518b3241404b8a02fa8
Andres Alba <bryanalba92@gmail.com>
2020-08-08 01:07:08 -0500

    Favorite restaurants

--- app/screens/Favorites.js
+++ app/screens/Favorites.js
@@ -1,6 +1,6 @@
 import React, {useState, useRef, useCallback} from 'react';
 import { StyleSheet, View, Text, FlatList, ActivityIndicator, TouchableOpacity } from 'react-native';
-import {Icon, Button} from 'react-native-elements';
+import {Icon, Button, Image} from 'react-native-elements';
 import {useFocusEffect} from '@react-navigation/native';
 import Loading from '../components/Loading';
 
@@ -110,12 +110,30 @@ function UserNoLogged(props) {
 
 function Restaurant(props) {
     const { restaurant } = props;
-    const { name } = restaurant.item;
+    const { name, images } = restaurant.item;
     return (
-        <View>
-            <Text>
-                {name}
-            </Text>
+        <View style={styles.restaurant}>
+            <TouchableOpacity onPress={() => console.log('ir')}>
+                <Image
+                    resizeMode='cover'
+                    style={styles.image}
+                    PlaceholderContent={<ActivityIndicator color='#fff' />}
+                    source={
+                        images[0] ? {uri: images[0]} : require('../../assets/img/no-image.png')
+                    }
+                />
+                <View style={styles.info}>
+                    <Text style={styles.name}>{name}</Text>
+                    <Icon
+                        type='material-community'
+                        name='heart'
+                        color='#f00'
+                        containerStyle={styles.favorite}
+                        onPress={() => console.log('remove')}
+                        underlayColor='transparent'
+                    />
+                </View>
+            </TouchableOpacity>
         </View>
     )
 }
@@ -128,5 +146,36 @@ const styles = StyleSheet.create({
     loaderRestaurants: {
         marginTop: 10,
         marginBottom: 10
+    },
+    restaurant: {
+        margin: 10
+    },
+    image: {
+        width: '100%',
+        height: 180
+    },
+    info: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        flexDirection: 'row',
+        paddingLeft: 20,
+        paddingRight: 20,
+        paddingTop: 10,
+        paddingBottom: 10,
+        marginTop: -30,
+        backgroundColor: '#fff'
+
+    },
+    name: {
+       fontWeight: 'bold',
+       fontSize: 30 
+    },
+    favorite: {
+        marginTop: -35,
+        backgroundColor: '#fff',
+        padding: 15,
+        borderRadius: 100
+
     }
 })
