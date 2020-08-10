8b0cb60344829f92d1728724e4430c7aeefc2508
Andres Alba <bryanalba92@gmail.com>
2020-08-10 11:35:03 -0500

    Login Facebbok

--- app/components/Account/LoginFacebook.js
+++ app/components/Account/LoginFacebook.js
@@ -1,18 +1,51 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { SocialIcon } from 'react-native-elements';
+import * as firebase from 'firebase';
+import * as Facebook from 'expo-facebook';
+import { useNavigation } from '@react-navigation/native';
+import { FacebookApi } from '../../utils/social';
+import Loading from '../Loading';
 
-export default function LoginFacebook() {
+export default function LoginFacebook(props) {
 
-    const login = () => {
-        console.log('Login..');
+    const { toastRef } = props;
+    const navigation = useNavigation();
+    const [loading, setLoading] = useState(false);
+
+    const login = async () => {
+        await Facebook.initializeAsync(FacebookApi.application_id);
+        const { type, token } = await Facebook.logInWithReadPermissionsAsync({permissions: FacebookApi.permissions})
+        
+        if (type === 'success') {
+            setLoading(true);
+            const credentials = firebase.auth.FacebookAuthProvider.credential(token);
+            firebase
+                .auth()
+                .signInWithCredential(credentials)
+                .then(() => {
+                    setLoading(false);
+                    navigation.navigate('account');
+                })
+                .catch(() => {
+                    setLoading(false);
+                    toastRef.current.show('Credenciales incorrectas');
+                })
+        } else if (type === 'cancel') {
+            toastRef.current.show('Inicio de sesion cancelado');
+        } else {
+            toastRef.current.show('Intentelo mas tarde');
+        }
     }
-    
+
     return (
+        <>
         <SocialIcon
             title='Iniciar sesion con Facebook'
             button
             type='facebook'
             onPress={login}
         />
+        <Loading isVisible={loading} text={'Iniciando sesion'} />
+        </>
     )
 }
---
+++ app/utils/social.js
@@ -0,0 +1,4 @@
+export const FacebookApi = {
+    application_id: '2913983622041367',
+    permissions: ['public_profile']
+}
