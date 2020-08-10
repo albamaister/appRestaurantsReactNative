d4acac95025c5a3f59ec75ed0fad88e385a06dd3
Andres Alba <bryanalba92@gmail.com>
2020-08-10 10:44:51 -0500

    Facebook Login button

--- App.js
+++ App.js
@@ -4,7 +4,14 @@ import Navigation from './app/navigations/Navigation';
 import { firebaseApp } from './app/utils/firebase';
 import {decode, encode} from 'base-64';
 
+// YellowBox.ignoreWarnings(['Setting a timer']);
 YellowBox.ignoreWarnings(['Setting a timer']);
+// const _console = _.clone(console);
+console.warn = message => {
+  if (message.indexOf('Setting a timer') <= -1) {
+    _console.warn(message);
+  }
+};
 if (!global.btoa) global.btoa = encode;
 if (!global.atob) global.atob = decode;
 
--- app/screens/Account/Login.js
+++ app/screens/Account/Login.js
@@ -4,6 +4,7 @@ import { Divider } from 'react-native-elements';
 import { useNavigation } from '@react-navigation/native';
 import LoginForm from '../../components/Account/LoginForm';
 import Toast from 'react-native-easy-toast';
+import LoginFacebook from '../../components/Account/LoginFacebook';
 
 export default function Login() {
     const toastRef = useRef();
@@ -18,7 +19,9 @@ export default function Login() {
                     <CreateAccount/>
                 </View>
                 <Divider style={styles.divider}/>
-                <Text>Social Login</Text>
+                <View style={styles.viewContainer}>
+                    <LoginFacebook/>
+                </View>
                 <Toast ref={toastRef} position='center' opacity={0.9}/>
         </ScrollView>
     )
