6357ebc699a4f63732dc159ed87d36fc9f74b632
Andres Alba <bryanalba92@gmail.com>
2020-08-10 01:16:00 -0500

    SearchBar from restaurants

--- app/screens/Search.js
+++ app/screens/Search.js
@@ -1,12 +1,37 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { View, Text, StyleSheet, FlatList, Image } from 'react-native';
+import {SearchBar, ListItem, Icon} from 'react-native-elements';
+import {FireSQL} from 'firesql';
+import firebase from 'firebase/app';
 
-export default function Search() {
+const fireSQL = new FireSQL(firebase.firestore(), { includeId: 'id' });
+
+export default function Search(props) {
+    const { navigation } = props;
+    const [search, setSearch] = useState('');
+    const [restaurants, setRestaurants] = useState([]);
+    useEffect(() => {
+        if (search) {
+            fireSQL.query(`SELECT  * FROM restaurants where name like '${search}%'`)
+                .then((response) => {
+                    setRestaurants(response);
+                })
+        }
+    }, [search])
     
     return (
         <View>
-            <Text>Search...</Text>
+            <SearchBar
+                placeholder='Busca tu restaurante'
+                onChangeText={(e) => setSearch(e)}
+                value={search}
+                containerStyle={styles.searchBar}                
+            />
         </View>
     )
-
 }
+const styles = StyleSheet.create({
+    searchBar: {
+        marginBottom: 20
+    }
+});
